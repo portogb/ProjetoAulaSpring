7f7cfb7d06f56a9145340951fa6d7556ce80e7ee
gabriel <porto.g4briel@gmail.com>
2022-11-10 23:16:22 -0300

    html para avaliacao criado

--- src/main/java/br/com/senac/controller/AvaliacaoController.java
+++ src/main/java/br/com/senac/controller/AvaliacaoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.senac.entity.Avaliacao;
@@ -11,7 +13,8 @@ import br.com.senac.service.AlunoService;
 import br.com.senac.service.AvaliacaoService;
 import br.com.senac.service.CursoService;
 
-@Controller("/avaliacoes")
+@Controller
+@RequestMapping("/avaliacoes")
 public class AvaliacaoController {
     
     @Autowired
@@ -25,22 +28,21 @@ public class AvaliacaoController {
 
     @GetMapping("/adiciona")
     public ModelAndView lista(){
-        ModelAndView mv = new ModelAndView("avaliacoes/inserir-nota");
+        ModelAndView mv = new ModelAndView("avaliacoes/inserirNota");
         mv.addObject("alunos", alunoService.buscarTodosAlunos());
         mv.addObject("cursos", cursoService.buscarTodosCursos());
         mv.addObject("avaliacao", new Avaliacao());
         return mv;
     }
 
-    @GetMapping("/save")
+    @PostMapping("/save")
     public ModelAndView save(@ModelAttribute("avaliacao") Avaliacao avaliacao){
         avaliacao.getAlunoCurso().setAluno(alunoService.buscarPorId(avaliacao.getAlunoCurso().getAluno().getId()));
         avaliacao.getAlunoCurso().setCurso(cursoService.buscarPorId(avaliacao.getAlunoCurso().getCurso().getId()));
         avaliacaoService.save(avaliacao);
-        ModelAndView mv = new ModelAndView("avaliacoes/listar-notas");
+        ModelAndView mv = new ModelAndView("avaliacoes/listarNotas");
         mv.addObject("avaliacoes", avaliacaoService.findAll());
         return mv;
     }
-
 
 }
